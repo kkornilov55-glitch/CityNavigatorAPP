620934f88a34d86df5a35719d0d5dd5be6c05b0f
Kirill <kkornilov55@gmail.com>
2026-04-20 19:32:13 +0300

    Добавьте файлы проекта.

---
+++ NavigatorLogic/Edge.cs
@@ -0,0 +1,9 @@
+﻿namespace NavigatorLogic
+{
+    public class Edge
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public double Length { get; set; }
+    }
+}

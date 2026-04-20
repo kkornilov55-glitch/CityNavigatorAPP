988509242939e7cf7530224be112c248c0a5dbe9
Kirill <kkornilov55@gmail.com>
2026-04-20 19:48:46 +0300

    Added Vertex class describing the location, name and id of the city. | Добавлен класс Vertex описывающий положение, название и id города.

---
+++ NavigatorLogic/Vertex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigatorLogic
+{
+    internal class Vertex
+    {
+        public int id {  get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public string Name { get; set; }
+
+    }
+}
